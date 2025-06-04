d5594b6bbcf4eb3b1b20fd1fbe7e0eac23346d48
Pratham <guptapratham27@gmail.com>
2025-06-04 20:56:08 +0530

    first commit

---
+++ Javateam/src/main/java/com/maven/test711/App.java
@@ -0,0 +1,13 @@
+package com.maven.test711;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
